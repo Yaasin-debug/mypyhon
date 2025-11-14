b9644591a9d61e9135ef8a1f3ebe78b0d83e103e
Yaasin-debug <sheikyaasin786@gmail.com>
2025-11-14 14:30:26 +0530

    Create Write a Python program to add a new element to the end of a list and remove one spacific element.py

---
+++ Write a Python program to add a new element to the end of a list and remove one spacific element.py
@@ -0,0 +1,7 @@
+#Write a Python program to add a new element to the end of a list and remove one spacific element
+numbers = [10, 20, 30, 40]
+
+numbers.append(50)      # add
+numbers.remove(20)      # remove
+
+print(numbers)

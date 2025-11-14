bb3c257fa770b7d0f56985ca707626c0d83a7132
Yaasin-debug <sheikyaasin786@gmail.com>
2025-11-14 14:34:57 +0530

    Create Write a Python program to sort a list in ascending order and descending order.py

---
+++ Write a Python program to sort a list in ascending order and descending order.py
@@ -0,0 +1,5 @@
+# Write a Python program to sort a list in ascending order and descending order.
+numbers = [50, 10, 40, 20, 30]
+
+print("Ascending:", sorted(numbers))
+print("Descending:", sorted(numbers, reverse=True))

609ac79a299e37c19ab147e3907ebfb386a03bcc
Yaasin-debug <sheikyaasin786@gmail.com>
2025-11-15 11:35:05 +0530

    Create inherititance.py

---
+++ inherititance.py
@@ -0,0 +1,11 @@
+#inherititance
+class hero:
+    def function (self):
+      print("pro")
+      class movie(bot):
+        def function (self):
+         print("bot")
+        obj=hero()
+        obj.function()
+        obj1=movie()
+        obj1.function()
